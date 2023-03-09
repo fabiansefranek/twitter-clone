5d193c6acff9d50fc0433118c58e1cc33a364c5f
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-09 14:28:51 +0100

    ✨ Format code

--- api/src/models/User.cs
+++ api/src/models/User.cs
@@ -2,12 +2,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace twitter_clone.Models
 {
-	public class User {
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public int Id { get; set; }
-		public string Username { get; set; }
-		public string Password { get; set; }
-		public string Role { get; set; }
-		public int CreatedAt { get; set; }
-	}
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int CreatedAt { get; set; }
+    }
 }
