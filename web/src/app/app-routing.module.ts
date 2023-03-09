3600d9f50e598418067b4bd6b2fc38d3395421fb
fabiansefranek <fabiansefranek@gmail.com>
2023-03-09 22:20:03 +0100

    ✨ Implement angular routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "login", component: LoginComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	declarations: [],
+	imports: [CommonModule, RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
