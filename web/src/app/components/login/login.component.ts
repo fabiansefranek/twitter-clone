6c79324b165f4457bcec3417554ad786cdd62882
fabiansefranek <fabiansefranek@gmail.com>
2023-03-09 23:46:28 +0100

    ✨ (web): Implement login component

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { User } from "src/types";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
+	user: User = {} as User;
+	constructor(private authService: AuthService) {}
 
+	handleLoginButtonClick() {
+		console.log(this.user);
+		this.authService.login(this.user);
+	}
 }
