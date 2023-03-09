ac3a1c330043098ae864de9f6fbe4a4c4d781598
fabiansefranek <fabiansefranek@gmail.com>
2023-03-09 23:44:41 +0100

    ✨ Import angular http client

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./components/app/app.component";
 import { LoginComponent } from "./components/login/login.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
 	declarations: [AppComponent, LoginComponent, LoginComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
