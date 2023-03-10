e41d8887d688c143bf0f0740a03dc5436d0c205f
fabiansefranek <fabiansefranek@gmail.com>
2023-03-10 11:03:34 +0100

    ✨ web: add access token utilities

--- web/src/types/index.d.ts
+++ web/src/types/index.d.ts
@@ -2,3 +2,11 @@ export type User = {
 	username: string;
 	password: string;
 };
+
+export type AccessToken = {
+	unique_name: string;
+	role: string;
+	exp: number;
+	nbf: number;
+	iat: number;
+};
