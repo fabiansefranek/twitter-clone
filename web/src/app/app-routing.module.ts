744255188365fbc43ca0a20c1bf644958c67975a
fabiansefranek <fabiansefranek@gmail.com>
2023-03-10 10:42:50 +0100

    ✨ web: add signup component and validate token

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
 
-const routes: Routes = [{ path: "login", component: LoginComponent }];
+const routes: Routes = [
+	{ path: "login", component: LoginComponent },
+	{ path: "signup", component: SignupComponent },
+];
 
 @NgModule({
 	declarations: [],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./components/app/app.component";
 import { LoginComponent } from "./components/login/login.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { SignupComponent } from "./components/signup/signup.component";
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, LoginComponent],
+	declarations: [AppComponent, LoginComponent, LoginComponent, SignupComponent],
 	imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
