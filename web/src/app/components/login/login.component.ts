bf6470e6adc91b5d84dccd31d6d38a1c1590dc8e
fabiansefranek <fabiansefranek@gmail.com>
2023-03-10 09:29:45 +0100

    ✨ web: redirect to home from login when already logged in

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { User } from "src/types";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-login",
@@ -9,10 +10,19 @@ import { User } from "src/types";
 })
 export class LoginComponent {
 	user: User = {} as User;
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService, private router: Router) {}
+
+	ngOnInit() {
+		const accessToken = window.sessionStorage.getItem("accessToken");
+		const tokenExpiry = window.sessionStorage.getItem("accessTokenExpiry");
+		const now = Math.floor(Date.now() / 1000);
+		if (accessToken && tokenExpiry && parseInt(tokenExpiry) > now) {
+			this.router.navigate(["/"]);
+		}
+	}
 
 	handleLoginButtonClick() {
-		console.log(this.user);
+		if (!this.user.username || !this.user.password) return;
 		this.authService.login(this.user);
 	}
 }
--- web/src/app/services/auth/auth.service.ts
+++ web/src/app/services/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { catchError, retry } from "rxjs/operators";
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { User } from "src/types";
+import { Router } from "@angular/router";
 
 @Injectable({
 	providedIn: "root",
 })
 export class AuthService {
 	private authUrl = "http://localhost:5000/auth/login"; //TODO: use env variable for port
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private router: Router) {}
 
 	login(user: User) {
 		const request = this.http
@@ -20,11 +21,12 @@ export class AuthService {
 			})
 			.subscribe({
 				next: (data: any) => {
-					if (data.token != null || data.token != undefined) {
-						const parsedToken = JSON.parse(atob(data.token.split(".")[1]));
-						console.log(new Date(parsedToken.exp * 1000));
-						console.log(new Date());
-						window.sessionStorage.setItem("bearer", data.token);
+					if (data.token != null) {
+						this.router.navigate(["/"]);
+						const token = JSON.parse(atob(data.token.split(".")[1]));
+						window.sessionStorage.setItem("accessToken", data.token);
+						window.sessionStorage.setItem("accessTokenExpiry", token.exp);
+						window.sessionStorage.setItem("username", token.username);
 					}
 				},
 				error: (err) => {
