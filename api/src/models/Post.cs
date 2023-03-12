7966d9c601b0066a0971e09768b387d7654b4231
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-12 10:22:44 +0100

    ✨ api: add post model

---
+++ api/src/models/Post.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace twitter_clone.Models;
+
+public class Post
+{
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
+	public User User { get; set; }
+	public int UserId { get; set; }
+	public string Text { get; set; }
+	public int CreatedAt { get; set; }
+	public int UpdatedAt { get; set; }
+}
