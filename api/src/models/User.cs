640ff11c8675814e70d6c37c5d300867ca6b9db9
fabiansefranek <fabiansefranek@gmail.com>
2023-03-12 20:48:16 +0100

    ⚰️ api: remove useless spaces

--- api/src/models/User.cs
+++ api/src/models/User.cs
@@ -6,9 +6,7 @@ namespace twitter_clone.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
         public string Username { get; set; }
-
         public string Password { get; set; }
         public string Role { get; set; }
         public int CreatedAt { get; set; }
