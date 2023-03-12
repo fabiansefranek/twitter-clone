f6c2b50d010f3673fb0b45ef38ad075a3d62ade3
fabiansefranek <fabiansefranek@gmail.com>
2023-03-12 20:47:35 +0100

    ⚰️ api: remove db injection into routes

--- api/src/Routes.cs
+++ api/src/Routes.cs
@@ -1,20 +1,10 @@
 using System.Security.Claims;
-using Microsoft.EntityFrameworkCore;
 using twitter_clone.controllers;
-using twitter_clone.Models;
-using twitter_clone.services;
 
 namespace twitter_clone;
 
 public class Routes
 {
-    private readonly TwitterCloneContext _db;
-
-    public Routes(TwitterCloneContext db)
-    {
-        _db = db;
-    }
-
     public void Mount(WebApplication app)
     {
         app.MapGet("/users", (TwitterCloneContext db) => db.Users.ToList());
@@ -28,6 +18,6 @@ public class Routes
         app.MapGet("/moderator", (ClaimsPrincipal user) => Results.Ok())
             .RequireAuthorization("moderator");
         app.MapGet("/posts", (TwitterCloneContext db) => db.Posts.ToList());
-        app.MapPost("/posts", PostController.AddPost);
+        app.MapPost("/posts", PostController.AddPost).RequireAuthorization("user");
     }
 }
