fa048250b77ed49c74c20a1f2ebdc00f4ce05018
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-16 14:43:25 +0100

    ✨ web: fetch posts on home screen

--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { PostComponent } from "../post/post.component";
-import { User } from "src/types";
+import { Post, User } from "src/types";
+import { PostService } from "src/app/services/post/post.service";
 
 @Component({
 	selector: "app-home",
@@ -10,10 +11,15 @@ import { User } from "src/types";
 })
 export class HomeComponent {
 	user: User = {} as User;
-	constructor(private authService: AuthService) {}
+	posts: Post[] = [];
+	constructor(private authService: AuthService, private postService: PostService) {}
 
 	ngOnInit() {
 		const user = this.authService.getUser();
 		if (user) this.user = user;
+		this.postService.getPosts().subscribe((posts) => {
+			console.log(posts);
+			this.posts = posts;
+		});
 	}
 }
