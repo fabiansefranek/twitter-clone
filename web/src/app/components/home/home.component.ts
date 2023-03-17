582bd918a70edf2ff90fec4a6df7a13d6595933e
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-17 20:03:58 +0100

    ✨ web: add post text area to home

--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -10,13 +10,18 @@ import { PostService } from "src/app/services/post/post.service";
 	styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
+	isLoggedIn: boolean = false;
 	user: User = {} as User;
 	posts: Post[] = [];
 	constructor(private authService: AuthService, private postService: PostService) {}
 
 	ngOnInit() {
-		const user = this.authService.getUser();
-		if (user) this.user = user;
+		const isLoggedIn = this.authService.isLoggedIn();
+		if (isLoggedIn) {
+			this.isLoggedIn = isLoggedIn;
+			const user = this.authService.getUser();
+			if (user) this.user = user;
+		}
 		this.postService.getPosts().subscribe((posts) => {
 			console.log(posts);
 			this.posts = posts;
