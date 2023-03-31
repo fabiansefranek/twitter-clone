cb0a4950217301f68663735a84c7a7552b938efe
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-31 21:23:10 +0200

    ✨ api: implement follow controller & update model

--- api/src/controllers/FollowController.cs
+++ api/src/controllers/FollowController.cs
@@ -1,64 +1,98 @@
 using System.Net;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using twitter_clone.Models;
 
 namespace twitter_clone.controllers;
 
 public class FollowController
 {
-	public static async Task<IResult> Follow([FromBody] User followed, User user, TwitterCloneContext db)
-	{
-		var followedUser = await db.Users.FindAsync(followed.Id);
-		if (followedUser == null) return Utils.Response("User to be followed does not exist", "", HttpStatusCode.NotFound);
+    public static async Task<IResult> Follow(int userId, User user, TwitterCloneContext db)
+    {
+        var followedUser = await db.Users.FindAsync(userId);
+        if (followedUser == null)
+            return Utils.Response(
+                "User to be followed does not exist",
+                "",
+                0,
+                HttpStatusCode.NotFound
+            );
 
-		var exists = db.Follows.Any(f => f.FollowedId == followedUser.Id && f.FollowerId == user.Id);
-		if (exists) return Utils.Response("", "", HttpStatusCode.NotFound);
+        var currentUser = await db.Users.FindAsync(user.Id);
+        if (currentUser == null)
+        {
+            return Utils.Response(
+                "Authenticated user does not exist",
+                "",
+                0,
+                HttpStatusCode.NotFound
+            );
+        }
 
-		var follow = new Follow()
-		{
-			Followed = new UserDTO(followedUser), FollowedId = followed.Id, Follower = new UserDTO(user), FollowerId = user.Id,
-			CreatedAt = Utils.GetTimestamp()
-		};
-		db.Follows.Add(follow);
-		await db.SaveChangesAsync();
-		return Utils.Response("", "", HttpStatusCode.OK);
-	}
+        var followExists = db.Follows.Any(
+            f => f.FollowedId == followedUser.Id && f.FollowerId == currentUser.Id
+        );
+        if (followExists)
+            return Utils.Response(
+                "Person to be followed was already followed",
+                "",
+                0,
+                HttpStatusCode.Conflict
+            );
 
-	public static async Task<IResult> Unfollow([FromBody] User unfollowed, User user, TwitterCloneContext db)
-	{
-		var unfollowedUser = await db.Users.FindAsync(unfollowed.Id);
-		if (unfollowedUser == null)
-		{
-			return Utils.Response("User to be unfollowed does not exist", "", HttpStatusCode.NotFound);
-		}
+        db.Follows.Add(new Follow(followed: followedUser, follower: currentUser));
+        await db.SaveChangesAsync();
 
-		var follow = await db.Follows.Where(f => f.FollowedId == unfollowedUser.Id && f.FollowerId == user.Id).FirstOrDefaultAsync();
-		if (follow == null) return Utils.Response("", "", HttpStatusCode.NotFound);
+        return Utils.Response("", "", 0, HttpStatusCode.OK);
+    }
 
-		db.Follows.Remove(follow);
-		await db.SaveChangesAsync();
-		return Utils.Response("", "", HttpStatusCode.OK);
-	}
+    public static async Task<IResult> Unfollow(int userId, User user, TwitterCloneContext db)
+    {
+        var unfollowedUser = await db.Users.FindAsync(userId);
+        if (unfollowedUser == null)
+            return Utils.Response(
+                "User to be unfollowed does not exist",
+                "",
+                0,
+                HttpStatusCode.NotFound
+            );
 
+        var currentUser = await db.Users.FindAsync(user.Id);
+        if (currentUser == null)
+        {
+            return Utils.Response(
+                "Authenticated user does not exist",
+                "",
+                0,
+                HttpStatusCode.NotFound
+            );
+        }
 
-	public static async Task<IResult> IsFollowing(int userId, User user, TwitterCloneContext db)
-	{
-		var isFollowing = await db.Follows.FirstOrDefaultAsync(f => f.FollowedId == userId && f.FollowerId == user.Id);
-		if (isFollowing == null)
-		{
-			return Utils.Response("", false, HttpStatusCode.OK);
-		}
-		else
-		{
-			return Utils.Response("", true, HttpStatusCode.OK);
-		}
-	}
+        var existingFollow = await db.Follows.FirstOrDefaultAsync(
+            f => f.FollowedId == unfollowedUser.Id && f.FollowerId == currentUser.Id
+        );
+        if (existingFollow == null)
+            return Utils.Response(
+                "Person to be unfollowed wasn't followed before",
+                "",
+                0,
+                HttpStatusCode.Conflict
+            );
 
-	public static async Task<IResult> GetFollows(User user, TwitterCloneContext db)
-	{
-		var follows = db.Follows.Where(f => f.FollowerId == user.Id);
-		return Utils.Response("", follows, HttpStatusCode.OK);
-	}
+        db.Follows.Remove(existingFollow);
+        await db.SaveChangesAsync();
+
+        return Utils.Response("", "", 0, HttpStatusCode.OK);
+    }
+
+    public static async Task<IResult> GetFollows(int userId, User user, TwitterCloneContext db)
+    {
+        var follows = await db.Follows.Where(f => f.FollowerId == userId).ToListAsync();
+        return Utils.Response("", follows, follows.Count, HttpStatusCode.OK);
+    }
+
+    public static async Task<IResult> GetFollowers(int userId, User user, TwitterCloneContext db)
+    {
+        var followers = await db.Follows.Where(f => f.FollowedId == userId).ToListAsync();
+        return Utils.Response("", followers, followers.Count, HttpStatusCode.OK);
+    }
 }
--- api/src/models/Follow.cs
+++ api/src/models/Follow.cs
@@ -4,11 +4,21 @@ namespace twitter_clone.Models;
 
 public class Follow
 {
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public int Id { get; set; }
-	public UserDTO Followed { get; set; }
-	public int FollowedId { get; set; }
-	public UserDTO Follower { get; set; }
-	public int FollowerId { get; set; }
-	public int CreatedAt { get; set; }
+    public Follow() { }
+
+    public Follow(User follower, User followed)
+    {
+        Follower = follower;
+        FollowerId = follower.Id;
+        Followed = followed;
+        FollowedId = followed.Id;
+    }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public User Follower { get; set; }
+    public int FollowerId { get; set; }
+    public User Followed { get; set; }
+    public int FollowedId { get; set; }
+    public int CreatedAt { get; set; }
 }
