3f13d26e45224aa4a88350b83e2676fa7cf544c4
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-31 21:26:03 +0200

    ✨ api: add count attribute to returns in auth controller

--- api/src/controllers/AuthenticationController.cs
+++ api/src/controllers/AuthenticationController.cs
@@ -16,6 +16,7 @@ public class AuthenticationController
             return Utils.Response(
                 "Username and/or password were not provided.",
                 "",
+                0,
                 HttpStatusCode.Conflict
             );
         var user = await db.Users.FirstOrDefaultAsync(u => u.Username.Equals(requestUser.Username));
@@ -23,16 +24,17 @@ public class AuthenticationController
             return Utils.Response(
                 "User with this username not found in database.",
                 "",
+                0,
                 HttpStatusCode.NotFound
             );
 
         var hashedPassword = AuthenticationService.HashString(requestUser.Password);
         if (!hashedPassword.Equals(user.Password))
-            return Utils.Response("Password is incorrect.", "", HttpStatusCode.Unauthorized);
+            return Utils.Response("Password is incorrect.", "", 0, HttpStatusCode.Unauthorized);
 
         var token = AuthenticationService.GenerateToken(user);
 
-        return Utils.Response("", new { token = token }, HttpStatusCode.OK);
+        return Utils.Response("", token, 1, HttpStatusCode.OK);
     }
 
     public static async Task<IResult> Signup([FromBody] User user, TwitterCloneContext db)
@@ -43,6 +45,7 @@ public class AuthenticationController
             return Utils.Response(
                 "User with that username already exists.",
                 "",
+                0,
                 HttpStatusCode.Conflict
             );
 
@@ -54,6 +57,7 @@ public class AuthenticationController
             return Utils.Response(
                 "Not all user details were provided. (username, password, fullname)",
                 "",
+                0,
                 HttpStatusCode.Conflict
             );
 
@@ -66,29 +70,20 @@ public class AuthenticationController
         await db.SaveChangesAsync();
 
         var token = AuthenticationService.GenerateToken(user);
-        return Utils.Response("", new { token = token }, HttpStatusCode.OK);
+        return Utils.Response("", token, 1, HttpStatusCode.OK);
     }
 
     public static IResult GetUserInfo(User user)
     {
-        return Utils.Response(
-            "",
-            new
-            {
-                id = user.Id,
-                username = user.Username,
-                role = user.Role
-            },
-            HttpStatusCode.OK
-        );
+        return Utils.Response("", user, 1, HttpStatusCode.OK);
     }
 
     public static IResult ValidateToken([FromBody] string token)
     {
         bool tokenIsValid = AuthenticationService.IsTokenValid(token);
         if (tokenIsValid)
-            return Utils.Response("", "", HttpStatusCode.OK);
+            return Utils.Response("", "", 0, HttpStatusCode.OK);
         else
-            return Utils.Response("Token is invalid.", "", HttpStatusCode.Unauthorized);
+            return Utils.Response("Token is invalid.", "", 0, HttpStatusCode.Unauthorized);
     }
 }
