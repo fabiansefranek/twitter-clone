88fa232692ea3eb0da49b5b2a86757bb5e6a78a8
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-31 21:25:32 +0200

    ✨ api: update post controller

--- api/src/controllers/PostController.cs
+++ api/src/controllers/PostController.cs
@@ -7,83 +7,108 @@ namespace twitter_clone.controllers;
 
 public class PostController
 {
-    public static async Task<IResult> AddPost(Post post, User user, TwitterCloneContext db)
+    public static async Task<IResult> CreatePost(Post post, User user, TwitterCloneContext db)
     {
-	    if (user.Id == -1) return Utils.Response("Token could not be parsed or no user information was found in the token provided.", "", HttpStatusCode.Conflict);
-
-        var userExists = await db.Users.FindAsync(user.Id) != null;
-        if (!userExists)
-            return Utils.Response($"User with ID {user.Id} was not found in the database.", "", HttpStatusCode.NotFound);
-
-        db.Posts.Add(
-            new Post
-            {
-                UserId = user.Id,
-                CreatedAt = Utils.GetTimestamp(),
-                Text = post.Text
-            }
-        );
+        db.Posts.Add(new Post(user: user, text: post.Text));
         await db.SaveChangesAsync();
 
-        return Utils.Response("", "", HttpStatusCode.OK);
+        return Utils.Response("", "", 0, HttpStatusCode.OK);
     }
 
-    public static IResult GetPosts(TwitterCloneContext db)
+    public static async Task<IResult> GetPosts(int id, TwitterCloneContext db)
     {
-	    var posts = db.Posts.OrderByDescending(p => p.CreatedAt).Include(p => p.User);
-	    var postsDto = posts.Select(post => new PostDTO(post));
-	    return Utils.Response("", postsDto, HttpStatusCode.OK);
+        List<Post> posts;
+        if (id == -1)
+        {
+            posts = await db.Posts
+                .OrderByDescending(p => p.CreatedAt)
+                .Include(p => p.User)
+                .ToListAsync();
+        }
+        else
+        {
+            posts = await db.Posts
+                .Where(p => p.UserId == id)
+                .OrderByDescending(p => p.CreatedAt)
+                .Include(p => p.User)
+                .ToListAsync();
+        }
+
+        var postsDto = posts.Select(post => new PostDTO(post));
+        return Utils.Response("", postsDto, postsDto.Count(), HttpStatusCode.OK);
     }
 
     public static async Task<IResult> GetPost(int id, TwitterCloneContext db)
     {
-	    var post = await db.Posts.Include(post => post.User).FirstOrDefaultAsync(post => post.Id == id);
-	    if (post == null)
-	    {
-		    return Utils.Response("Post with this id was not found", "", HttpStatusCode.NotFound);
-	    }
+        var post = await db.Posts
+            .Include(post => post.User)
+            .FirstOrDefaultAsync(post => post.Id == id);
+        if (post == null)
+        {
+            return Utils.Response(
+                "Post with this id was not found",
+                "",
+                0,
+                HttpStatusCode.NotFound
+            );
+        }
 
-	    return Utils.Response("",
-		    new PostDTO(post),
-		    HttpStatusCode.OK
-	    );
+        return Utils.Response("", new PostDTO(post), 1, HttpStatusCode.OK);
     }
 
-    public static async Task<IResult> DeletePost([FromBody] Post requestedPost, User user, TwitterCloneContext db)
+    public static async Task<IResult> DeletePost(
+        int id,
+        User user,
+        TwitterCloneContext db
+    )
     {
-	    var post = await db.Posts.FirstOrDefaultAsync(p => p.Id == requestedPost.Id);
-	    if (post == null)
-	    {
-		    return Utils.Response("Post not found", "", HttpStatusCode.NotFound);
-	    }
+        var post = await db.Posts.FindAsync(id);
+        if (post == null)
+        {
+            return Utils.Response("Post not found", "", 0, HttpStatusCode.NotFound);
+        }
 
-	    if (post.UserId != user.Id)
-	    {
-		    return Utils.Response("Post not from authenticated user", "", HttpStatusCode.Forbidden);
-	    }
+        if (user.Role != "moderator" && post.UserId != user.Id)
+        {
+            return Utils.Response(
+                "Post not from authenticated user",
+                "",
+                0,
+                HttpStatusCode.Forbidden
+            );
+        }
 
-	    db.Posts.Remove(post);
-	    await db.SaveChangesAsync();
+        db.Posts.Remove(post);
+        await db.SaveChangesAsync();
 
-	    return Utils.Response("", "", HttpStatusCode.OK);
+        return Utils.Response("", "", 0, HttpStatusCode.OK);
     }
 
-    public static async Task<IResult> UpdatePost([FromBody] Post requestedPost, User user, TwitterCloneContext db)
+    public static async Task<IResult> UpdatePost(
+        [FromBody] Post updatedPost,
+        User user,
+        TwitterCloneContext db
+    )
     {
-	    var post = await db.Posts.FirstOrDefaultAsync(p => p.Id == requestedPost.Id);
-	    if (post == null)
-	    {
-		    return Utils.Response("Post not found", "", HttpStatusCode.NotFound);
-	    }
+        var post = await db.Posts.FirstOrDefaultAsync(p => p.Id == updatedPost.Id);
+        if (post == null)
+        {
+            return Utils.Response("Post not found", "", 0, HttpStatusCode.NotFound);
+        }
 
-	    if (post.UserId != user.Id)
-	    {
-		    return Utils.Response("Post not from authenticated user", "", HttpStatusCode.Forbidden);
-	    }
+        if (post.UserId != user.Id)
+        {
+            return Utils.Response(
+                "Post not from authenticated user",
+                "",
+                0,
+                HttpStatusCode.Forbidden
+            );
+        }
 
-	    db.Posts.Update(post);
-	    await db.SaveChangesAsync();
+        db.Posts.Update(post);
+        await db.SaveChangesAsync();
 
-	    return Utils.Response("", "", HttpStatusCode.OK);
+        return Utils.Response("", "", 0, HttpStatusCode.OK);
     }
 }
