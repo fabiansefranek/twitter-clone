8b8a9a35eaa70b50768f71912993a130716d17ef
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-31 21:25:01 +0200

    ✨ api: add likes and follows to user model

--- api/src/models/User.cs
+++ api/src/models/User.cs
@@ -12,5 +12,7 @@ namespace twitter_clone.Models
         public string Role { get; set; }
         public int CreatedAt { get; set; }
         public List<Post> Posts { get; set; }
+        public List<Like> Likes { get; set; }
+        public List<Follow> Follows { get; set; }
     }
 }
