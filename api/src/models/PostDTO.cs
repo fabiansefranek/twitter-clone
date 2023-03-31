fbc52c54b7680be9de8d54f92410461fa10788e8
Fabian Sefranek <fabiansefranek@gmail.com>
2023-03-31 21:21:12 +0200

    ✨ api: update dto models

--- api/src/models/PostDTO.cs
+++ api/src/models/PostDTO.cs
@@ -2,18 +2,22 @@ namespace twitter_clone.Models;
 
 public class PostDTO
 {
-	public PostDTO() {}
-	public PostDTO(Post post)
-	{
-		Id = post.Id;
-		User = new UserDTO(post.User);
-		Text = post.Text;
-		CreatedAt = post.CreatedAt;
-		UpdatedAt = post.UpdatedAt;
-	}
-	public int Id { get; set; }
-	public UserDTO User { get; set; }
-	public string Text { get; set; }
-	public int CreatedAt { get; set; }
-	public int UpdatedAt { get; set; }
+    public PostDTO() { }
+
+    public PostDTO(Post post)
+    {
+        Id = post.Id;
+        User = new UserDTO(post.User);
+        UserId = post.UserId;
+        Text = post.Text;
+        CreatedAt = post.CreatedAt;
+        UpdatedAt = post.UpdatedAt;
+    }
+
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public UserDTO User { get; set; }
+    public string Text { get; set; }
+    public int CreatedAt { get; set; }
+    public int UpdatedAt { get; set; }
 }
--- api/src/models/UserDTO.cs
+++ api/src/models/UserDTO.cs
@@ -2,19 +2,21 @@ namespace twitter_clone.Models
 {
     public class UserDTO
     {
-	    public UserDTO() {}
-	    public UserDTO(User user)
-	    {
-		    Id = user.Id;
-		    Fullname = user.Fullname;
-		    Username = user.Username;
-		    Role = user.Role;
-		    CreatedAt = user.CreatedAt;
-	    }
-	    public int Id { get; set; }
-	    public string Fullname { get; set; }
-	    public string Username { get; set; }
-	    public string Role { get; set; }
-	    public int CreatedAt { get; set; }
+        public UserDTO() { }
+
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            Fullname = user.Fullname;
+            Username = user.Username;
+            Role = user.Role;
+            CreatedAt = user.CreatedAt;
+        }
+
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public int CreatedAt { get; set; }
     }
 }
