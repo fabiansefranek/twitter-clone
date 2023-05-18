902889a5c30a856211f3c5c316638af980e2ced8
Fabian Sefranek <fabiansefranek@gmail.com>
2023-05-18 11:33:00 +0200

    ✨ web: create profile event emitter

--- web/src/app/services/user/user.service.ts
+++ web/src/app/services/user/user.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable, Output } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Post, User } from "src/types";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
 	providedIn: "root",
 })
 export class UserService {
 	private userUrl = `${environment.BASE_URL}/users`;
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private authService: AuthService) {}
+	@Output() fetchUser = new EventEmitter();
 
 	getUser(username: string): Observable<User> {
 		return this.http.get<User>(`${this.userUrl}/${username}`);
@@ -17,5 +19,9 @@ export class UserService {
 
 	getPostsFromUser(userId: string): Observable<Post[]> {
 		return this.http.get<Post[]>(`${this.userUrl}/${userId}/posts`);
+	}
+
+	updateUser(user: Partial<User>): Observable<User> {
+		return this.http.put<User>(`${this.userUrl}`, user);
 	}
 }
